d593129eda5a5fc8c74435d324858f329eab6665
blanton <theblanton@gmail.com>
2011-06-05 23:05:52 -0400

    converted Maze to an Actor
    broken input for now

--- src/com/mentaldistortion/react/Maze.java
+++ src/com/mentaldistortion/react/Maze.java
@@ -13,11 +13,17 @@ import com.badlogic.gdx.physics.box2d.*;
 
 import java.io.InputStream;
 
-public class Maze implements InputProcessor, Disposable, QueryCallback
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.scenes.scene2d.*;
+
+public class Maze
+    extends Actor
+    implements Disposable
+    //implements InputProcessor, Disposable, QueryCallback
 {
     final static String TAG = "React::Maze";
 
-    Camera cam;
+    //Camera cam;
 
     float dt;
 
@@ -33,11 +39,11 @@ public class Maze implements InputProcessor, Disposable, QueryCallback
         selectedBody = null;
     }
 
-    Maze (Camera cam)
+    Maze ()
     {
-        Fixture f = null;
+        super("Maze");
 
-        this.cam = cam;
+        Fixture f = null;
 
         dt = 1.0f / 60.0f;
 
@@ -54,27 +60,6 @@ public class Maze implements InputProcessor, Disposable, QueryCallback
         }
     }
 
-    static float expMovAvg (float avg, float newValue, float n)
-    {
-        float alpha = 2.0f / (n + 1.0f);
-        float retval = (newValue * alpha) + (avg * (1.0f - alpha));
-        return retval;
-    }    
-
-    Vector2 lastAcc = new Vector2();
-
-    void update ()
-    {
-        dt = expMovAvg(dt, Gdx.graphics.getDeltaTime(), 60);
-        world.step(dt, 3, 3);
-        world.clearForces();
-    }
-
-    void render ()
-    {
-        debugRenderer.render(world);
-    }
-
     @Override
     public void dispose ()
     {
@@ -82,116 +67,156 @@ public class Maze implements InputProcessor, Disposable, QueryCallback
         world.dispose();
     }
 
+    void update (float dt)
+    {
+        world.step(dt, 3, 3);
+        world.clearForces();
+    }
+
     @Override
-    public boolean keyTyped (char character)
+    protected void draw (SpriteBatch batch, float parentAlpha)
+    {
+        GL10 gl = Gdx.graphics.getGL10();
+        gl.glDisable(gl.GL_TEXTURE_2D);
+
+        debugRenderer.render(world);
+    }
+
+
+    @Override
+    public Actor hit (float x, float y)
+    {
+        return null;
+    }
+
+    @Override
+    protected boolean touchDown (float x, float y, int pointer) 
     {
         return false;
     }
 
     @Override
-    public boolean keyDown (int keyCode)
+    protected boolean touchDragged (float x, float y, int pointer) 
     {
         return false;
     }
 
     @Override
-    public boolean keyUp (int keyCode)
+    protected boolean touchUp (float x, float y, int pointer) 
     {
         return false;
     }
 
-    @Override
-    public boolean scrolled (int amount)
-    {
-        return false;
-    }
+//    @Override
+//    public boolean keyTyped (char character)
+//    {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean keyDown (int keyCode)
+//    {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean keyUp (int keyCode)
+//    {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean scrolled (int amount)
+//    {
+//        return false;
+//    }
 
-    @Override
-    public boolean touchDown (int x, int y, int pointer, int button)
-    {
-        boolean retval = false;
+//    @Override
+//    public boolean touchDown (int x, int y, int pointer, int button)
+//    {
+//        boolean retval = false;
+//
+//        Ray ray = cam.getPickRay(x, y);
+//
+//        float delta = 0.0f;
+//
+//        world.QueryAABB(this,
+//                      ray.origin.x - delta,
+//                      ray.origin.y - delta,
+//                      ray.origin.x + delta,
+//                      ray.origin.y + delta);
+//
+//        // store prev
+//        Vector3 v = new Vector3(x, y, 0);
+//        cam.unproject(v);
+//        prevWorldPos = new Vector2(v.x, v.y);
+//
+//        if (selectedBody != null) {
+//            retval = true;
+//        }
+//
+//        return retval;
+//    }
 
-        Ray ray = cam.getPickRay(x, y);
+//    @Override
+//    public boolean touchDragged (int x, int y, int pointer)
+//    {
+//        boolean retval = false;
+//
+//        // store prev
+//        Vector3 v = new Vector3(x, y, 0);
+//        cam.unproject(v);
+//        Vector2 curWorldPos = new Vector2(v.x, v.y);
+//
+//        if (selectedBody != null) {
+//            Vector2 delta = prevWorldPos.sub(curWorldPos);
+//            Vector2 p = selectedBody.getPosition();
+//            p = p.sub(delta);
+//            selectedBody.setTransform(p, selectedBody.getAngle());
+//            retval = true;
+//        }
+//
+//        // store prev
+//        prevWorldPos = curWorldPos;
+//
+//        return retval;
+//    }
 
-        float delta = 0.0f;
-
-        world.QueryAABB(this,
-                      ray.origin.x - delta,
-                      ray.origin.y - delta,
-                      ray.origin.x + delta,
-                      ray.origin.y + delta);
-
-        // store prev
-        Vector3 v = new Vector3(x, y, 0);
-        cam.unproject(v);
-        prevWorldPos = new Vector2(v.x, v.y);
-
-        if (selectedBody != null) {
-            retval = true;
-        }
-
-        return retval;
-    }
-
-    @Override
-    public boolean touchDragged (int x, int y, int pointer)
-    {
-        boolean retval = false;
-
-        // store prev
-        Vector3 v = new Vector3(x, y, 0);
-        cam.unproject(v);
-        Vector2 curWorldPos = new Vector2(v.x, v.y);
-
-        if (selectedBody != null) {
-            Vector2 delta = prevWorldPos.sub(curWorldPos);
-            Vector2 p = selectedBody.getPosition();
-            p = p.sub(delta);
-            selectedBody.setTransform(p, selectedBody.getAngle());
-            retval = true;
-        }
-
-        // store prev
-        prevWorldPos = curWorldPos;
-
-        return retval;
-    }
-
-    @Override
-    public boolean touchUp (int x, int y, int pointer, int button)
-    {
-        boolean retval = false;
-
-        if (selectedBody != null) {
-            selectedBody = null;
-            retval = true;
-        }
-
-        // store prev
-        prevWorldPos = null;
-
-        return retval;
-    }
-
-    @Override
-    public boolean touchMoved (int x, int y)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean reportFixture (Fixture fixture)
-    {
-        Gdx.app.log(TAG, fixture.toString() + ": " + fixture.getUserData());
-        boolean toContinue = true;
-
-        if (fixture.getBody().getType() == BodyDef.BodyType.StaticBody) {
-            Gdx.app.log(TAG, "manipulate the above");
-            toContinue = false;
-            selectedBody = fixture.getBody();
-        }
-
-        return toContinue;
-    }
+//    @Override
+//    public boolean touchUp (int x, int y, int pointer, int button)
+//    {
+//        boolean retval = false;
+//
+//        if (selectedBody != null) {
+//            selectedBody = null;
+//            retval = true;
+//        }
+//
+//        // store prev
+//        prevWorldPos = null;
+//
+//        return retval;
+//    }
+//
+//    @Override
+//    public boolean touchMoved (int x, int y)
+//    {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean reportFixture (Fixture fixture)
+//    {
+//        Gdx.app.log(TAG, fixture.toString() + ": " + fixture.getUserData());
+//        boolean toContinue = true;
+//
+//        if (fixture.getBody().getType() == BodyDef.BodyType.StaticBody) {
+//            Gdx.app.log(TAG, "manipulate the above");
+//            toContinue = false;
+//            selectedBody = fixture.getBody();
+//        }
+//
+//        return toContinue;
+//    }
 
 }
--- src/com/mentaldistortion/react/React.java
+++ src/com/mentaldistortion/react/React.java
@@ -3,98 +3,179 @@ package com.mentaldistortion.react;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.*;
 
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.actors.*;
+
 import com.badlogic.gdx.math.Vector3;
 
-public class React implements ApplicationListener
+public class React
+    implements ApplicationListener
 {
     final static String TAG = "React::React";
 
-    GL10 gl;
-    OrthographicCamera cam;
     FPSLogger fpsLogger;
+
+    Stage stage;
     Maze maze;
+
+    Stage ui;
+    Group topBar;
+
+    float dt;
+
+    {
+        dt = 1.0f / 60.0f;
+    }
+
+    static float expMovAvg (float avg, float newValue, float n)
+    {
+        float alpha = 2.0f / (n + 1.0f);
+        float retval = (newValue * alpha) + (avg * (1.0f - alpha));
+        return retval;
+    }
 
     @Override
     public void create ()
     {
-        cam = new OrthographicCamera();
+        GL10 gl = Gdx.graphics.getGL10();
 
-        fpsLogger = new FPSLogger();
-        gl = Gdx.graphics.getGL10();
+        stage = new Stage(Gdx.graphics.getWidth(),
+                          Gdx.graphics.getHeight(),
+                          false);
 
+        // stage ui
+        ui = new Stage(Gdx.graphics.getWidth(),
+                       Gdx.graphics.getHeight(),
+                       false);
+
+        topBar = new Group("topBar");
+        ui.addActor(topBar);
+
+        // OpenGL
         gl.glClearColor(0, 0, 0, 0);
 
-        maze = new Maze(cam);
-
+        // input multiplexing
         InputMultiplexer inputMultiplexer = new InputMultiplexer();
-        inputMultiplexer.addProcessor(maze);
-        /// @bug Drag scrolling is really jerky
-        inputMultiplexer.addProcessor(new InputAdapter() {
-            Vector3 prevWorldPos;
-            {
-                prevWorldPos = null;
-            }
-            @Override
-            public boolean scrolled (int amount)
-            {
-                cam.position.y += amount * 0.02;
-                return true;
-            }
-            @Override
-            public boolean touchDragged (int x, int y, int pointer)
-            {
-                if (prevWorldPos == null) {
-                    return false;
-                }
-                Vector3 curWorldPos = new Vector3(x, y, 0);
-                cam.unproject(curWorldPos);
-
-                Vector3 deltaWorldPos = prevWorldPos.sub(curWorldPos);
-
-                cam.position.add(deltaWorldPos);
-
-                prevWorldPos = curWorldPos;
-                return true;
-            }
-            @Override
-            public boolean touchDown (int x, int y, int pointer, int button)
-            {
-                prevWorldPos = new Vector3(x, y, 0);
-                cam.unproject(prevWorldPos);
-                return true;
-            }
-            @Override
-            public boolean touchUp (int x, int y, int pointer, int button)
-            {
-                prevWorldPos = null;
-                return true;
-            }
-        });
         Gdx.input.setInputProcessor(inputMultiplexer);
+        inputMultiplexer.addProcessor(stage);
+        inputMultiplexer.addProcessor(ui);
+
+        // button
+        Button playButton = new Button(
+            "playButton",
+            new Texture(Gdx.files.internal("ui/play.png"))
+            );
+        playButton.clickListener = new Button.ClickListener()
+        {
+            @Override
+            public void clicked (Button button)
+            {
+                Gdx.app.log(TAG, "play");
+            }
+        };
+        topBar.addActor(playButton);
+        Gdx.app.log(TAG, playButton.toString());
+
+        // maze
+        maze = new Maze();
+        stage.addActor(maze);
+
+//
+//        fpsLogger = new FPSLogger();
+//        gl = Gdx.graphics.getGL10();
+//
+//
+//        maze = new Maze(cam);
+//
+//        /// @bug Drag scrolling is really jerky
+//        inputMultiplexer.addProcessor(new InputAdapter() {
+//            Vector3 prevWorldPos;
+//            {
+//                prevWorldPos = null;
+//            }
+//            @Override
+//            public boolean scrolled (int amount)
+//            {
+//                cam.position.y += amount * 0.02;
+//                return true;
+//            }
+//            @Override
+//            public boolean touchDragged (int x, int y, int pointer)
+//            {
+//                if (prevWorldPos == null) {
+//                    return false;
+//                }
+//                Vector3 curWorldPos = new Vector3(x, y, 0);
+//                cam.unproject(curWorldPos);
+//
+//                Vector3 deltaWorldPos = prevWorldPos.sub(curWorldPos);
+//
+//                cam.position.add(deltaWorldPos);
+//
+//                prevWorldPos = curWorldPos;
+//                return true;
+//            }
+//            @Override
+//            public boolean touchDown (int x, int y, int pointer, int button)
+//            {
+//                prevWorldPos = new Vector3(x, y, 0);
+//                cam.unproject(prevWorldPos);
+//                return true;
+//            }
+//            @Override
+//            public boolean touchUp (int x, int y, int pointer, int button)
+//            {
+//                prevWorldPos = null;
+//                return true;
+//            }
+//        });
 
     }
 
     @Override
     public void render ()
     {
-        //fpsLogger.log();
+        GL10 gl = Gdx.graphics.getGL10();
 
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-        cam.update();
-        cam.apply(gl);
+        dt = expMovAvg(dt, Gdx.graphics.getDeltaTime(), 10);
 
-        maze.update();
-        maze.render();
+        maze.update(dt);
+
+        stage.act(dt);
+        stage.draw();
+
+        ui.act(dt);
+        ui.draw();
+
+        //fpsLogger.log();
     }
 
     @Override
     public void resize (int w, int h)
     {
-        float aspect = ((float)h) / ((float)w);
+        GL10 gl = Gdx.graphics.getGL10();
 
-        cam.viewportWidth  = maze.size.x * 2.0f;
-        cam.viewportHeight = maze.size.x * 2.0f * aspect;
+        gl.glViewport(0, 0, w, h);
+
+        // stage
+        float aspect = ((float)h) / ((float)w);
+        stage.setViewport(maze.size.x * 2.0f,
+                          maze.size.x * 2.0f * aspect,
+                          false);
+
+        /// @bug is there a better way to fix the camera position?
+        Camera cam = stage.getCamera();
+        cam.position.set(0, 0, 0);
+
+        // ui
+        ui.setViewport(w, h, false);
+
+        // position bars
+        /// @bug find a way around the hard coded values
+        topBar.height = 64;
+        topBar.y = h - topBar.height;
     }
 
     @Override
@@ -110,7 +191,7 @@ public class React implements ApplicationListener
     @Override
     public void dispose ()
     {
-        maze.dispose();
+//        maze.dispose();
     }
 
 }
